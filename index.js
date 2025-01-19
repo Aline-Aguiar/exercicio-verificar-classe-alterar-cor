0a437ff00b64e8565312c88f5bb9221eae9dc2ca
Aline-Aguiar <aline.aglilly@gmail.com>
2025-01-18 22:58:14 -0300

    codificando o arquivo index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const quadrado = document.getElementsByClassName("quadrado")[0];
+
+quadrado.addEventListener("click", trocarCor);
+
+// function trocarCor(){
+//     if(quadrado.classList.contains("preto")){
+//         quadrado.classList.remove("preto");
+//         quadrado.classList.add("azul");
+//     }else if(quadrado.classList.contains("azul")){
+//         quadrado.classList.remove("azul");
+//         quadrado.classList.add("preto");
+//     }
+
+function trocarCor(){
+    if(quadrado.classList.contains("azul")){
+        quadrado.classList.add("preto");
+        quadrado.classList.remove("azul");
+    }else{
+        quadrado.classList.remove("preto");
+        quadrado.classList.add("azul");
+    }
+}
